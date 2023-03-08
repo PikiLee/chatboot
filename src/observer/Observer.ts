08cc0adb26a314c8528c6d33256d48925e52a570
luckypeppa <871565198@qq.com>
2023-03-08 09:12:22 +0800

    refactor: remove observer

--- src/observer/Observer.ts
+++
@@ -1,3 +0,0 @@
-export interface Observer {
-	update(): void
-}
--- src/observer/Subject.ts
+++
@@ -1,7 +0,0 @@
-import { Observer } from './Observer'
-
-export interface Subject {
-	registerObserver(observer: Observer): void
-	removeObserver(observer: Observer): void
-	notifyObservers(): void
-}
